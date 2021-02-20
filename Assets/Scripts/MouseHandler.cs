e1c57976275a626ee3738da581e79db3018c6501
Niko <70172259+NikoCSUMB@users.noreply.github.com>
2021-02-19 16:14:28 -0800

    Raycast + Canvas Starters

---
+++ Assets/Scripts/MouseHandler.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)){
+            RaycastHit hit;
+            Ray mouse = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(mouse, out hit)){
+                Debug.Log("We hit something???" + hit.collider.name);
+
+
+                // Ideally you would call some generic "onHit" function for the respective block, but this works for now.
+                if(hit.collider.name == "Brick"){ 
+                    Destroy(hit.collider.gameObject);
+                }
+                if (hit.collider.name == "Question"){
+                    // TODO: Add coin 
+                }
+
+            }
+        }
+    }
+}
